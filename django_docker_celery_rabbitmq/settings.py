74a594e996fa99eb04710a862edd2ed7c2f6ff6a
goranaviani <goranaviani@gmail.com>
2019-11-11 19:37:02 +0100

    periodic task

--- django_docker_celery_rabbitmq/settings.py
+++ django_docker_celery_rabbitmq/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'test_app.apps.TestAppConfig',
+
 ]
 
 MIDDLEWARE = [
@@ -118,3 +120,17 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+
+from celery.schedules import crontab
+
+
+#beat settings for fun with args
+CELERY_BEAT_SCHEDULE = {
+      'task-periodic_task': {
+       'task': 'test_app.tasks.periodic_task',
+        #'schedule': crontab(minute="*/30"),
+        'schedule': 2,
+        
+    },
+}
